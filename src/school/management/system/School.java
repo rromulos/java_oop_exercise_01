fedae1b6f16e799dc6439c509f74e304bd13d02f
Rômulo von Santos <rromulosp@gmail.com>
2022-11-23 22:15:12 -0300

    adds first classes

---
+++ src/school/management/system/School.java
@@ -0,0 +1,12 @@
+package school.management.system;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class School {
+	
+	private List<Teacher> teachers;
+	private List<Student> students;
+	private BigDecimal totalMoneyEarned;
+	private BigDecimal totalMoneySpent;
+}
---
+++ src/school/management/system/Student.java
@@ -0,0 +1,23 @@
+package school.management.system;
+
+import java.math.BigDecimal;
+
+/**
+ * 
+ * @author romulo
+ * This class is responsible for keep the track of students
+ *
+ */
+public class Student {
+	
+	private int id;
+	private String name;
+	private int grade;
+	private BigDecimal feesPaid;
+	private BigDecimal feedTotal;
+	
+	public Student(int id, String name, BigDecimal feedPaid, BigDecimal feedTotal) {
+		
+	}
+
+}
